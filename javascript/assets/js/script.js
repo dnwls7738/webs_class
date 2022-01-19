3f141d99ead0eeb4af1e246ad98002c690fc0c8d
dnwls7738 <97070013+dnwls7738@users.noreply.github.com>
2022-01-19 10:23:39 +0900

    20220119

--- javascript/assets/js/script.js
+++ javascript/assets/js/script.js
@@ -1,16 +1,5 @@
-document.querySelector(".result").addEventListener("click", function(){
-    document.querySelectorAll()
-})
-//for(let i=1; i<=17; i++){
-//    document.querySelector(".result).addEventListener("click", function(){
-//        document.querySelectorAll(".result"+i)
-//    });
-//}
-
-// document.querySelectorAll(".result h4").forEach(function(el){
-//     // console.log(el)
-//     el.addEventListener("click", function(){
-//         alert("클릭");
-//     })
-// });
-
+document.querySelectorAll(".result").forEach(function(el){
+    el.addEventListener("click", function(){
+        el.classList.toggle("active");
+    });
+});
