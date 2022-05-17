03bcc4ead2f835106c88a584b5027381b4f68362
dnwls7738 <cjsdnwls7738@gmail.com>
2022-05-17 18:44:55 +0900

    20220517

---
+++ portfolio/assets/game/musicplayer/api/music.js
@@ -0,0 +1,123 @@
+
+let allMusic = [
+    {
+        name : "Unknown",
+        artist : "Unknown",
+        img : "Frame 44",
+        audio : "music1",
+    },
+    {
+        name : "Everybody Knows",
+        artist : "Leonard Cohen",
+        img : "Frame 45",
+        audio : "music2",
+    },
+    {
+        name : "Extreme Ways",
+        artist : "Moby",
+        img : "Frame 46",
+        audio : "music3",
+    },
+    {
+        name : "Butterflies",
+        artist : "Sia",
+        img : "Frame 47",
+        audio : "music4",
+    },
+    {
+        name : "The Final Victory",
+        artist : "Haggard",
+        img : "Frame 48",
+        audio : "music5",
+    },
+    {
+        name : "Genius",
+        artist : "LSD",
+        img : "Frame 49",
+        audio : "music6",
+    },
+    {
+        name : "The Comeback kid",
+        artist : "Lindi Ortega",
+        img : "Frame 50",
+        audio : "music7",
+    },
+    {
+        name : "Overdose",
+        artist : "Grandson",
+        img : "Frame 51",
+        audio : "music8",
+    },
+    {
+        name : "Human",
+        artist : "Rag'n'Bone Man",
+        img : "Frame 52",
+        audio : "music9",
+    },
+    {
+        name : "Unknown",
+        artist : "Unknown",
+        img : "Frame 53",
+        audio : "music10",
+    },
+    {
+        name : "Unknown",
+        artist : "Unknown",
+        img : "Frame 54",
+        audio : "music11",
+    },
+    {
+        name : "Unknown",
+        artist : "Unknown",
+        img : "Frame 55",
+        audio : "music12",
+    },
+    {
+        name : "Unknown",
+        artist : "Unknown",
+        img : "Frame 56",
+        audio : "music13",
+    },
+    {
+        name : "Unknown",
+        artist : "Unknown",
+        img : "Frame 57",
+        audio : "music14",
+    },
+    {
+        name : "Unknown",
+        artist : "Unknown",
+        img : "Frame 58",
+        audio : "music15",
+    },
+    {
+        name : "Unknown",
+        artist : "Unknown",
+        img : "Frame 59",
+        audio : "music16",
+    },
+    {
+        name : "Unknown",
+        artist : "Unknown",
+        img : "Frame 60",
+        audio : "music17",
+    },
+    {
+        name : "Unknown",
+        artist : "Unknown",
+        img : "Frame 61",
+        audio : "music18",
+    },
+    {
+        name : "Unknown",
+        artist : "Unknown",
+        img : "Frame 62",
+        audio : "music19",
+    },
+    {
+        name : "Unknown",
+        artist : "Unknown",
+        img : "Frame 63",
+        audio : "music20",
+    }
+]
